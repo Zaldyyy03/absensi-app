a21373e4a181ea0950ec2ad29750966af84168bc
Zaldyyy03 <kurniadiredjarizaldy@gmail.com>
2025-06-25 13:07:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_ENDPOINT = "https://script.google.com/macros/s/AKfycbytp8Z-j51BFaNpsiPATBfzjYstMnIcIyvBTyUGSXeFFwitjTBz6SnFn69ZRWzsLWxe/exec";
+const API_ENDPOINT = "https://script.google.com/macros/s/AKfycbxWpmoOZhYuTHzFKJWnEZ-mihXE2mhTSkPApJDhyRykQOYtPaA57gcMaVMXzjQgcPmw/exec";
